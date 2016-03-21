378a5fa07753da60f0eb9b9fdcb468f9ee4a32d5
Michael Sturrus <msturrus@gmail.com>
2016-03-21 12:58:39 -0500

    missing commas are no fun missing commas hurt someone

--- db/bustr_migration.sql
+++ db/bustr_migration.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE beta_bustr;
 \c beta_bustr
-CREATE TABLE account (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255) password_hash VARCHAR(255));
+CREATE TABLE account (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), password_hash VARCHAR(255));
 CREATE TABLE pet_info (id SERIAL PRIMARY KEY, species VARCHAR(255), size DECIMAL, color VARCHAR(255));
 CREATE TABLE pet_name (id SERIAL PRIMARY KEY, name VARCHAR(255), most_popular VARCHAR(255));
--- db/kreatr_migration.sql
+++ db/kreatr_migration.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE beta_kreatr;
 \c beta_kreatr
-CREATE TABLE account (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255) password_hash VARCHAR(255));
+CREATE TABLE account (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), password_hash VARCHAR(255));
 CREATE TABLE comic_text (id SERIAL PRIMARY KEY, story VARCHAR(255), background VARCHAR(255));
 CREATE TABLE picture (id SERIAL PRIMARY KEY, name VARCHAR(255), link VARCHAR(255));
