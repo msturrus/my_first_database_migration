460185f46c33211e32580116e2b87e1f7face132
Michael Sturrus <msturrus@gmail.com>
2016-03-21 13:23:25 -0500

    Some of the id's were VARCHARS now changed to SERIAL PRIMARY ID

--- db/ringit_migration.sql
+++ db/ringit_migration.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE beta_ringit;
 \c beta_ringit
 CREATE TABLE account (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), password_hash VARCHAR(255));
-CREATE TABLE phone_plan (id VARCHAR(255), company VARCHAR(255), plan VARCHAR(255), cost DECIMAL);
-CREATE TABLE reccomendation (id VARCHAR(255), company VARCHAR(255), plan VARCHAR(255), link VARCHAR(255));
+CREATE TABLE phone_plan (id SERIAL PRIMARY KEY, company VARCHAR(255), plan VARCHAR(255), cost DECIMAL);
+CREATE TABLE reccomendation (id SERIAL PRIMARY KEY, company VARCHAR(255), plan VARCHAR(255), link VARCHAR(255));
